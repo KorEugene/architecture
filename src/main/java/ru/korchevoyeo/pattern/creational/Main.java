06830aa89e28484f90192736a635c2991386175e
KorEugene <johnkorch@yandex.ru>
2021-12-13 21:22:18 +0300

    logic corrections

--- src/main/java/ru/korchevoyeo/pattern/creational/Main.java
+++ src/main/java/ru/korchevoyeo/pattern/creational/Main.java
@@ -8,15 +8,18 @@ import ru.korchevoyeo.pattern.creational.factory.MessageFactory;
 import ru.korchevoyeo.pattern.creational.factory.SmsFactory;
 import ru.korchevoyeo.pattern.creational.jdo.Client;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        ClientMessageDto initialData = initData();
+        List<ClientMessageDto> initialData = initData();
 
         System.out.println("Create SMS DTO: ");
-        System.out.println(createMessage(createFactory(MessageType.SMS), initialData));
+        System.out.println(createMessage(createFactory(initialData.get(0).getType()), initialData.get(0)));
 
         System.out.println("Create EMAIL DTO: ");
-        System.out.println(createMessage(createFactory(MessageType.EMAIL), initialData));
+        System.out.println(createMessage(createFactory(initialData.get(1).getType()), initialData.get(1)));
 
     }
 
@@ -35,7 +38,8 @@ public class Main {
         return factory.getMessage(initialData);
     }
 
-    private static ClientMessageDto initData() {
-        return new ClientMessageDto(new Client("Ivanov Ivan", "89111111111", "ivan@mail.ru"), "testMessage");
+    private static List<ClientMessageDto> initData() {
+        return Arrays.asList(new ClientMessageDto(new Client("Ivanov Ivan", "89111111111", "ivan@mail.ru"), MessageType.SMS, "test sms"),
+                new ClientMessageDto(new Client("Petrov Petr", "89222222222", "petr@mail.ru"), MessageType.EMAIL, "test email"));
     }
 }
--- src/main/java/ru/korchevoyeo/pattern/creational/dto/ClientMessageDto.java
+++ src/main/java/ru/korchevoyeo/pattern/creational/dto/ClientMessageDto.java
@@ -1,18 +1,25 @@
 package ru.korchevoyeo.pattern.creational.dto;
 
+import ru.korchevoyeo.pattern.creational.enums.MessageType;
 import ru.korchevoyeo.pattern.creational.jdo.Client;
 
 public class ClientMessageDto {
     private final Client client;
+    private final MessageType type;
     private final String message;
 
-    public ClientMessageDto(Client client, String message) {
+    public ClientMessageDto(Client client, MessageType type, String message) {
         this.client = client;
+        this.type = type;
         this.message = message;
     }
 
     public Client getClient() {
         return client;
+    }
+
+    public MessageType getType() {
+        return type;
     }
 
     public String getMessage() {
